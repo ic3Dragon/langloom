b66466fe69603123c036ac44379081a16106e6a6
ic3Dragon <catherine.oestlund@gmail.com>
2024-04-02 11:35:53 +0200

    Update(file.js): Add filePath validation + user notifications with color

--- src/file.js
+++ src/file.js
@@ -1,23 +1,39 @@
 const fs = require('fs');
+const colors = require('colors');
+
+const validateFile = (filePath) => {
+  if (!filePath.match(/.json$/i)) {
+    console.log(colors.red(`Can only import json: ${filePath}`));
+    process.exit(1);
+  }
+  if (!fs.existsSync(filePath)) {
+    console.log(colors.red(`File was not found: ${filePath}`));
+    process.exit(1);
+  }
+}
 
 async function processLocalData(localFilePath) {
+  validateFile(localFilePath);
+  console.info(colors.yellow('Processing local json file...'));
     try {
         const localData = JSON.parse(fs.readFileSync(localFilePath, 'utf8'));
         return localData;
     } catch (error) {
-        console.error(`Error processing local data: ${error.message}`);
+      console.error(colors.red(`Error processing local data: ${error.message}`));
         return { error: error.message };
     }
 }
 
 async function processLocalKeys(localFilePath) {
+  validateFile(localFilePath);
+  console.info(colors.yellow('Processing local keys...'));
     try {
         const localData = JSON.parse(fs.readFileSync(localFilePath, 'utf8'));
         const localKeys = [];
         await traverse(localData, [], localKeys);
         return { keys: localKeys };
     } catch (error) {
-        console.error(`Error processing local keys: ${error.message}`);
+      console.error(colors.red(`Error processing local keys: ${error.message}`));
         return { error: error.message };
     }
 }
